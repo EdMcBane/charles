958ec03341e44793dea01b377da0cc7d907cd365
Francesco Degrassi <francesco.degrassi@optionfactory.net>
2021-11-03 12:14:20 +0100

    :new: first commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+const WIDTH: usize = 256;
+const HEIGHT: usize = 256;
+
+fn main() {
+    println!("P3");
+    println!("{} {}", WIDTH, HEIGHT);
+    println!("{}", u8::MAX);
+    for j in (0..WIDTH).rev() {
+        for i in 0..HEIGHT {
+            let (r, g, b) = (
+                (255.999 * i as f32 / (WIDTH - 1) as f32) as u8,
+                (255.999 * j as f32 / (WIDTH - 1) as f32) as u8,
+                (255.999 * 0.25) as u8);
+            println!("{} {} {}", r, g, b);
+        }
+    }
+}
