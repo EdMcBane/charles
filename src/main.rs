042a0779a8131ea58ac4b7a3b77fcffa4c92528b
Francesco Degrassi <francesco.degrassi@optionfactory.net>
2021-11-03 14:12:49 +0100

    :new: vec type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,86 @@
+use std::ops::{Neg, Index, AddAssign, MulAssign, DivAssign, IndexMut};
+
 const WIDTH: usize = 256;
 const HEIGHT: usize = 256;
+
+struct Vec3([f32; 3]);
+
+impl Default for Vec3 {
+    fn default() -> Self {
+        Vec3([0., 0., 0.])
+    }
+}
+
+impl Vec3 {
+    fn new(x: f32, y: f32, z: f32) -> Self {
+        Vec3([x, y, z])
+    }
+
+    fn x(self) -> f32 {
+        self.0[0]
+    }
+    fn y(self) -> f32 {
+        self.0[1]
+    }
+    fn z(self) -> f32 {
+        self.0[2]
+    }
+
+    fn length_squared(&self) -> f32 {
+        self.0.into_iter().map(|e| e.powi(2)).sum()
+    }
+
+    fn length(&self) -> f32 {
+        self.length_squared().sqrt()
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Vec3([-self.0[0], -self.0[1], -self.0[2]])
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        self.0.index(index)
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        self.0.index_mut(index)
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        for (l, r) in self.0.iter_mut().zip(rhs.0.into_iter()) {
+            *l += r;
+        }
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, t: f32) {
+        for l in self.0.iter_mut() {
+            *l *= t;
+        }
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, t: f32) {
+        for l in self.0.iter_mut() {
+            *l /= t;
+        }
+    }
+}
 
 fn main() {
     println!("P3");
